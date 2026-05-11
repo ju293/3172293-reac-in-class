7342b12fe2cf228b54c93314b3e6eef11c29f5db
ju293 <jt338852@gmail.com>
2026-05-11 11:57:07 -0500

    chore(base): codigo base para las actividades desarrolladas en las sesiones de formacion

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="min-h-scren bg-green-800 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">
+        con rico programar con tailwind v4 funciona full ...
+      </h1>
+    </div>
+  );
+}
